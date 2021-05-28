88029c71d0a267bd3050abeafeaa7aeb96b6f927
nestoreo <32349925+nestoreo@users.noreply.github.com>
2021-05-27 19:39:14 -0700

    progress

--- Game.java
+++ Game.java
@@ -4,8 +4,155 @@ import java.io.*;
 
 public class Game{
 
+	//First team is home team and second is away
+	private Team[] teams = new Team[2];
+
+	//average rest days of teams
+	private double restDays;
+
+	private Player[] starters = new Player[10];
+
+	private double spread;
+
+	private double overUnder;
+
+	private double moneyLine;
+
+	private double totalScore;
+
+	//number of features
 	private final int numFeatures=1;
-	private ArrayList features = new ArrayList();
+
+	//the values of each feature
+	private boolean[] values;
+
+	//True is over and false is under
 	private boolean label;
+
+
+	//constructor for the player class
+	public Game(String[] game, ArrayList<Player> allPlayers, ArrayList<Team> allTeams)
+	{
+		//Home team
+		teams[0] = getTeam(game[1], allTeams);
+		//Away Team
+		teams[1] = getTeam(game[0], allTeams);
+		//Rest Day Average
+		restDays = restDaysAverage(game[2],game[3]);
+		//starters first five on home team second five away team
+		starters = getPlayers(game, allPlayers);
+
+
+	}
+
+	//Returns the teams where index 0 is home team and index 1 is away team
+	public Team[] getTeams()
+	{
+		return teams;
+	}
+	//Returns the teams where index 0 is home team and index 1 is away team
+	public double getRestDays()
+	{
+		return restDays;
+	}
+	public Player[] getStarters()
+	{
+		return starters;
+	}
+
+
+
+
+
+
+	//Takes in a team name and list of team objects and returns the correct team object
+	private static Player[] getPlayers(String[] game, ArrayList<Player> allPlayers)
+	{
+		Player[] players = new Player[10];
+
+		//From index 4-13 are the starters
+		for (int i=0;i<10;i++)
+		{
+			System.out.println(game[i+4]);
+			//Normalizing string to avoid conflicts
+			String playerName = OverUnderClassifier.normalizeName(game[i+4]);
+			for (Player player:allPlayers)
+			{
+				if (player.getName().contains(playerName))
+				{
+					if(i%2==0)
+					{
+						players[Integer.valueOf(i/2)] = player;
+					}
+					else{
+						players[Integer.valueOf(i/2+5)] = player;
+					}
+					break;
+				}
+			}
+		}
+		for (Player i:players)
+		{
+			System.out.println(player.getName());
+		}
+
+
+		//Checking which team and returning it
+		return players;
+	}
+
+	//Takes in a team name and list of team objects and returns the correct team object
+	private static Team getTeam(String teamName, ArrayList<Team> allTeams)
+	{
+		//Normalizing string to avoid conflicts
+		teamName = OverUnderClassifier.normalizeName(teamName);
+
+		//Checking which team and returning it
+		for (Team team:allTeams)
+		{
+			if (team.getTeamName().contains(teamName))
+			{
+				return team;
+			}
+		}
+		return null;
+	}
+
+	private static double restDaysAverage(String firstTeam, String secondTeam)
+	{
+		double restFirst;
+		double restSecond;
+
+		if(firstTeam.equals("3+"))
+			restFirst = 3.0;
+		else if(firstTeam.equals("2"))
+			restFirst = 2.5;
+		else if(firstTeam.equals("1"))	
+			restFirst = 2.0;
+		else if(firstTeam.equals("3IN4"))
+			restFirst = 1.5;
+		else if(firstTeam.equals("B2B"))
+			restFirst = 1.0;
+		else
+			restFirst = 0.5;
+
+
+		if(secondTeam.equals("3+"))
+			restSecond = 3.0;
+		else if(secondTeam.equals("2"))
+			restSecond = 2.5;
+		else if(secondTeam.equals("1"))	
+			restSecond = 2.0;
+		else if(secondTeam.equals("3IN4"))
+			restSecond = 1.5;
+		else if(secondTeam.equals("B2B"))
+			restSecond = 1.0;
+		else
+			restSecond = 0.5;
+
+		return (restFirst+restSecond)/2;
+	}
+
+	
 
 }
--- OverUnderClassifier.java
+++ OverUnderClassifier.java
@@ -10,7 +10,13 @@ public class OverUnderClassifier {
 	static Random random = new Random();
 
 	//Constants
-	static ArrayList<Player> allPlayers= new ArrayList();
+
+	//AllPlayers that started game
+	static ArrayList<Player> allPlayers;
+	//All teams
+	static ArrayList<Team> allTeams;
+
+
 
 	static int exampleCount;
 	static int featureCount;
@@ -22,41 +28,115 @@ public class OverUnderClassifier {
 	//processing data
 	public static void main(String[] args) throws FileNotFoundException {
 
-
 		classifyExamples("2018-2019-Box-Scores.txt");
 	}
 
 	private static void classifyExamples(String data) throws FileNotFoundException {
 
 		//Populating stats of all players
-		loadPlayers("2018-2019-Player-Stats.txt");
-
-		ArrayList<Game> trainOver =  loadOvers("2018-2019-Box-Scores-train.txt");
-		ArrayList<Game> trainUnder =  loadUnders("2018-2019-Box-Scores-train.txt");
-
-
+		allPlayers = loadPlayers("2018-2019-Player-Stats.txt");
+		
+		//Populating stats of all teams
+		allTeams = loadTeams("2018-2019-Team-Stats.txt");
+		
+		//Populating all games in the training set
+		ArrayList<Game> trainingGames = loadGames("2018-2019-Box-Scores-train.txt");
 	}
 	
+	private static ArrayList<Game> loadGames(String data) throws FileNotFoundException {
+
+		Scanner scan = new Scanner(new File(data));
+
+		ArrayList<Game> allGames = new ArrayList();
+
+
+		while (scan.hasNextLine())
+		{
+			String line1 = scan.nextLine();
+			String line2 = scan.nextLine();
+
+			String[] gameSplit1 = line1.split("\t");
+			String[] gameSplit2 = line2.split("\t");
+			
+			String[] game = new String[gameSplit1.length*2];
+			for (int i=0;i<gameSplit1.length;i++)
+			{
+				
+				game[2*i]=gameSplit1[i];
+				game[2*i+1]=gameSplit2[i];
+			}
+			
+			allGames.add(new Game(game, allPlayers, allTeams));
+		}
+
+		for (Game g: allGames)
+		{
+			System.out.println("Home: "+g.getTeams()[0].getTeamName()+" vs. Away: "+g.getTeams()[1].getTeamName());
+			System.out.println("Rest Day Combined: "+g.getRestDays());
+			for (Player player:g.getStarters())
+			{
+				System.out.println("Starter: "+player.getName());
+			}
+		}
+
+		scan.close();
+		return allGames;
+	}
+
+	//reads the dataset with statistics of all teams and creates a team object for each
+	//and adds it to the allTeams arraylist
+	private static ArrayList<Team> loadTeams(String dataset) throws FileNotFoundException {
+
+		//needed to read dataset
+		Scanner scan = new Scanner(new File(dataset));
+
+		ArrayList<Team> teams = new ArrayList<Team>();
+
+		while (scan.hasNextLine())
+		{
+			//Getting team stat line
+			String teamLine = scan.nextLine();
+			//Getting each individual stat
+			String[] teamStats = teamLine.split("\t");
+			//normalizing team name
+			teamStats[1] = normalizeName(teamStats[1]);
+			//Adding team to allTeams
+			teams.add(new Team(teamStats));
+		}
+		scan.close();
+
+		return teams;
+	}
 
 
 
-	private static void loadPlayers(String dataset) throws FileNotFoundException {
 
+	//reads the dataset with statistics of all players of that season and creates a player object for each
+	//and adds it to the allPlayers arraylist
+	private static ArrayList<Player> loadPlayers(String dataset) throws FileNotFoundException {
+
+		//needed to read dataset
 		Scanner scan = new Scanner(new File(dataset));
 
 		//Because the data creates multiple rows if a player played for multiple teams that season
 		//we need to keep track of that and make sure we do not add the same player multiple times
 		ArrayList<String> duplicates = new ArrayList<String>();
 
+		ArrayList<Player> players = new ArrayList<Player>();
+
 		while (scan.hasNextLine())
 		{
+			//Getting player stat line
 			String playerLine = scan.nextLine();
+			//Getting each individual stat
 			String[] playerStats = playerLine.split("\t");
 
-			//Checking that we have not added the player already
+			//Checking that we have not added the player already and that they did start a game
 			if ((!duplicates.contains(playerStats[0])) &&(!playerStats[5].equals("0"))){
+				//normalizing name
 				playerStats[1] = normalizeName(playerStats[1]);
-				allPlayers.add(new Player(playerStats));
+				//Adding player to starters
+				players.add(new Player(playerStats));
 			}
 
 			//Adding the player to duplicate if they will have duplicates
@@ -67,58 +147,23 @@ public class OverUnderClassifier {
 		}
 		scan.close();
 
-
+		return players;
 	}
 
 
-     private static ArrayList<Game> loadOvers(String data) throws FileNotFoundException {
 
-		Scanner scan = new Scanner(new File(data));
-
-		while (scan.hasNextLine())
-		{
-			String game = scan.nextLine();
-			String[] gameSplits = game.split("\t");
-			System.out.println(gameSplits);
-		}
-		scan.close();
-		return null;
-
-	}
-
-
-	private static ArrayList<Game> loadUnders(String data) throws FileNotFoundException {
-
-		Scanner scan = new Scanner(new File(data));
-
-		while (scan.hasNextLine())
-		{
-			String game = scan.nextLine();
-			String[] gameSplits = game.split("\t");
-			for (String s : gameSplits)
-			{
-				System.out.print(s+"|");
-			}
-			System.out.println("");
-			
-		}
-		for (Player s : allPlayers)
-		{
-			System.out.print(s.getOPM());
-		}
-		scan.close();
-		return null;
-	}
 
 
 	//Cleaning the name to avoid dealing with accents, special characters, etc when mapping
-	//from other datasets
-	private static String normalizeName(String name){
+	//from other datasets -- normalizeName(Álex Abrines\abrinal01) -> alexabrines
+	public static String normalizeName(String name){
 
+		//Removing white space and making lower case
 		name = name.replace(" ","").toLowerCase();
+		//Needed to replace accented characters with normal characters
 		name = Normalizer.normalize(name, Normalizer.Form.NFD);
-		name = name.replaceAll("[^\\p{ASCII}]", "");
-		name = name.split("[^a-z0-9]")[0].replace(" ","").replaceAll("\\p{Punct}", "");
+		//Replacing special character and removing the player id
+		name = name.replaceAll("[^\\p{ASCII}]", "").split("[^a-z0-9]")[0].replace(" ","").replaceAll("\\p{Punct}", "");
 
 		return name;
 
